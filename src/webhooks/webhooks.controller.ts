f4e83c0294932d72bf0ddc29817b4416524b008a
Luis Cruz <luisccc99@gmail.com>
2026-04-24 09:07:31 -0600

    Eliminar logs que muestran informacion confidencial

--- src/webhooks/webhooks.controller.ts
+++ src/webhooks/webhooks.controller.ts
@@ -32,8 +32,6 @@ export class WebhooksController {
       const signature = req.headers['x-signature'];
       const requestId = req.headers['x-request-id'];
 
-      this.logger.log({ signature, requestId });
-
       if (!signature || !requestId) {
         this.logger.log(`Ignoring malformed req no signature or requestId`)
         return;
@@ -47,12 +45,12 @@ export class WebhooksController {
       const dataId = req.body?.data?.id;
       const action = req.body?.action;
       const type = req.body?.type;
-      this.logger.log(`body: ${JSON.stringify(req.body)}`);
+
       if (!timestamp || !hash || !dataId) {
         this.logger.log(`Ignoring malformed req: ${JSON.stringify({ action, type, donacionType })} (no ts hash or dataid)`)
         return 'received';
       }
-      this.logger.log({ dataId, timestamp, hash })
+
       const manifest = this.webhookService.getManifestString(dataId, requestId as string, timestamp);
       const isReqAuthentic = () => this.webhookService.isHashValid(hash, manifest, donacionType);
 
